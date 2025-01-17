b4807a847603783da7667dad200fa04da3c24ef1
Mohellebi-cmd <mohellebinourelislam@gmail.com>
2025-01-17 18:18:18 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,15 @@
+function checkAnswer() {
+    let correctAnswer = "4";
+    let selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+    let feedback = document.getElementById('feedback');
+    let userAnswer = selectedAnswer.value;
+
+
+    if (userAnswer=== correctAnswer) {
+        feedback.textContent = 'Correct!';
+    } else {
+        feedback.textContent = 'Incorrect. The correct answer is 4.';
+    }
+}
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
 
